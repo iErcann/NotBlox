a517423f76bad387a99dbe8be46e38b26dd15012
iErcann <ercanfr@protonmail.com>
2025-07-18 18:23:35 +0200

    testing twitter

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -9,6 +9,48 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="3D Game on 𝕏 (Notblox.online)" />
+      <meta property="og:description" content="3D Multiplayer Game - click to interact!" />
+      <meta property="og:url" content="https://www.notblox.online/" />
+      <meta property="og:image" content="https://www.notblox.online/PreviewTestGame.webp" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:image:alt" content="3D Multiplayer Game" />
+      <meta
+        property="og:image:secure_url"
+        content="https://www.notblox.online/PreviewTestGame.webp"
+      />
+      <meta property="og:image:type" content="image/webp" />
+      <meta property="og:site_name" content="NotBlox" />
+      <meta property="og:locale" content="en_US" />
+      {/* <meta property="og:video" content="https://grokgames.vercel.app/demo.webm" />
+      <meta property="og:video:type" content="video/webm" />
+      <meta property="og:video:width" content="640" />
+      <meta property="og:video:height" content="360" />
+      <meta property="og:video:duration" content="10" />
+      <meta property="og:video:tag" content="3D" />
+      <meta property="og:video:tag" content="Interactive" />
+      <meta property="og:video:tag" content="Three.js" /> */}
+
+      {/* Twitter Card Meta Tags */}
+      <meta name="twitter:card" content="player" />
+      <meta name="twitter:site" content="@iercan" />
+      <meta name="twitter:title" content="3D Multiplayer Game on 𝕏 (iercan_)" />
+      <meta name="twitter:description" content="3D Multiplayer Game - click to interact!" />
+      <meta name="twitter:url" content="https://www.notblox.online/" />
+      <meta name="twitter:image" content="https://www.notblox.online/PreviewTestGame.webp" />
+      <meta name="twitter:image:width" content="1200" />
+      <meta name="twitter:image:height" content="630" />
+      <meta name="twitter:image:alt" content="3D Multiplayer Demo" />
+      <meta name="twitter:player" content="https://www.notblox.online/" />
+      <meta name="twitter:player:width" content="480" />
+      <meta name="twitter:player:height" content="480" />
+      <meta name="twitter:app:name:iphone" content="iErcan" />
+      <meta name="twitter:app:name:ipad" content="iErcan" />
+      <meta name="twitter:app:name:googleplay" content="iErcan" />
+      <meta name="twitter:creator" content="@iErcan" />
+
       <body className="bg-gray-50">
         <main>{children}</main>
       </body>
--- front/components/GameHud.tsx
+++ front/components/GameHud.tsx
@@ -22,6 +22,7 @@ export default function GameHud({
   gameInstance,
 }: GameHudProps) {
   const messagesEndRef = useRef<HTMLDivElement>(null)
+  const smallerMessagesEndRef = useRef<HTMLDivElement>(null)
   const refContainer = useRef<HTMLDivElement>(null)
   const [notifications, setNotifications] = useState<
     Array<{ id: number; content: string; author: string; timestamp: number }>
@@ -30,6 +31,7 @@ export default function GameHud({
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' })
+    smallerMessagesEndRef.current?.scrollIntoView({ behavior: 'smooth' })
   }
 
   useEffect(() => {
@@ -146,31 +148,54 @@ export default function GameHud({
           </div>
         ))}
       </div>
-      <div className="flex justify-between items-center">
-        <div className="shadow-4xl p-4 rounded-lg space-y-1 bg-gray-800 bg-opacity-20">
-          <p className="text-sm">👋 Welcome to </p>
-          <a
-            className="text-sm md:text-2xl font-bold pointer-events-auto hover:text-gray-400"
-            href="/"
-          >
-            NotBlox<span className="font-medium">.online</span>
-          </a>
-          <div className="text-sm flex justify-center text-white pointer-events-auto">
-            <Link
-              href="https://discord.gg/kPhgtj49U2"
-              target="_blank"
-              className="flex items-center justify-center mt-2 gap-2 px-3 py-1 w-full bg-transparent border border-b-2 hover:bg-blue-900/30 rounded-md transition-colors"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 640 512"
-                className="h-5 w-5 text-white fill-white"
+      <div className="flex flex-col pointer-events-auto h-48 w-48   md:hidden">
+        {/* Chat messages */}
+        <div className="overflow-y-auto flex-1 space-y-2 pr-2  h-10">
+          {getFilteredMessages().map((messageComponent, index) => {
+            return (
+              <div
+                key={index}
+                ref={index === getFilteredMessages().length - 1 ? smallerMessagesEndRef : null}
               >
-                <path d="M524.5 69.8a1.5 1.5 0 0 0 -.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 0 0 -1.9 .9 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 0 0 0 -15.1-30.6 1.9 1.9 0 0 0 -1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 0 -.8 .7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0 -1-2.6 321.2 321.2 0 0 1 -45.9-21.9 1.9 1.9 0 0 1 -.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9 .2c2.9 2.4 6 4.9 9.1 7.2a1.9 1.9 0 0 1 -.2 3.1 301.4 301.4 0 0 1 -45.9 21.8 1.9 1.9 0 0 0 -1 2.6 391.1 391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1 .7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 311 447.5 337.6 417.9 337.6z" />
-              </svg>
-              <span className="font-medium">Join Discord</span>
-            </Link>
-          </div>
+                <div
+                  className={`rounded-lg p-2 ${
+                    messageComponent.messageType === SerializedMessageType.TARGETED_CHAT
+                      ? 'bg-gray-900 bg-opacity-40'
+                      : 'bg-gray-700 bg-opacity-30'
+                  }`}
+                >
+                  <p className="text-sm break-words">
+                    <span
+                      className={`font-medium ${
+                        messageComponent.messageType === SerializedMessageType.TARGETED_CHAT
+                          ? 'text-gray-1000'
+                          : ''
+                      }`}
+                    >
+                      {messageComponent.author}
+                    </span>
+                    : {messageComponent.content}
+                  </p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+
+        {/* Input */}
+        <div className="mt-2">
+          <input
+            type="text"
+            placeholder="Type your message..."
+            className="w-full p-2 bg-gray-700 bg-opacity-30 text-white rounded-lg focus:outline-none"
+            onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+              if (e.key === 'Enter') {
+                sendMessage(e.currentTarget.value)
+                e.currentTarget.value = ''
+                e.currentTarget.blur()
+              }
+            }}
+          />
         </div>
       </div>
 
