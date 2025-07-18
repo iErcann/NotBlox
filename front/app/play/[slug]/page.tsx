230e41a20682997195e3c21f3e3c92965143459d
iErcann <ercanfr@protonmail.com>
2025-07-18 18:32:05 +0200

    test

--- front/app/play/[slug]/page.tsx
+++ front/app/play/[slug]/page.tsx
@@ -1,7 +1,6 @@
 // app/play/[slug]/page.tsx
 import gameData from '../../../public/gameData.json'
 import { GameInfo } from '@/types'
-import { Metadata } from 'next'
 import GameContent from '@/components/GameContent'
 
 export async function generateStaticParams(): Promise<{ slug: string }[]> {
@@ -22,30 +21,6 @@ function getGamesBySlug(slug: string): GameInfo {
 
 // https://nextjs.org/docs/app/building-your-application/upgrading/version-15#params--searchparams
 type Params = Promise<{ slug: string }>
-
-export async function generateMetadata({ params }: { params: Params }): Promise<Metadata> {
-  const { slug } = await params
-  const gameInfo = getGamesBySlug(slug)
-
-  return {
-    title: `Play ${gameInfo.title} - NotBlox`,
-    description: gameInfo.metaDescription,
-    openGraph: {
-      title: `Play ${gameInfo.title} - NotBlox`,
-      description: gameInfo.metaDescription,
-      images: gameInfo.images ?? [],
-      siteName: 'NotBlox Online',
-    },
-    twitter: {
-      card: 'summary_large_image',
-      site: '@iercan_',
-      creator: '@iercan_',
-    },
-    alternates: {
-      canonical: `https://www.notblox.online/play/${gameInfo.slug}`,
-    },
-  }
-}
 
 export default async function GamePage({ params }: { params: Params }) {
   const { slug } = await params
